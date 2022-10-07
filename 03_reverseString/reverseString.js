60b4174bb2d9ba3c2a253b2c8f03643daa22899b
Lxran <lxranman@gmail.com>
2022-10-08 00:15:32 +0300

    completed reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let reverse = string.split("").reverse();
+  let result = "";
+  for (let i = 0; i < string.length; i++) {
+    result += reverse[i]
+  }
+  return result;
 };
 
 // Do not edit below this line
