ab0dae5887aa8804568782eb5d5a75c8c4b7ef38
Lxran <lxranman@gmail.com>
2022-10-08 19:28:24 +0300

    completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let normalized = string.replace(/[^\w\s\']|_/g, "").replace(/\s/g, "").toLowerCase();
+  let array1 = normalized.split("");
+  let array2 = normalized.split("").reverse();
+  for (let i = 0; i < normalized.length; ++i) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
