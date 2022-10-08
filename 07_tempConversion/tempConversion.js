4e44e018dbd3bd1ed11c772593a9085b8d581326
Lxran <lxranman@gmail.com>
2022-10-08 13:26:42 +0300

    completed tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let f = ((temp - 32) * (5/9));
+  if (Number.isInteger(f)) {
+    return f
+  }
+  return Number.parseFloat(f.toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let c = (temp * (9/5) + 32);
+  if (Number.isInteger(c)) {
+    return c
+  }
+  return Number.parseFloat(c.toFixed(1))
 };
 
 // Do not edit below this line
