4a60b60711c6c926df904d65f7f4133aea479999
Lxran <lxranman@gmail.com>
2022-10-08 12:21:39 +0300

    completed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  let numbers = [];
+  if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) {
+    return "ERROR"
+  } else {
+    for (let i = 0; i <= Math.max(start, end) - Math.min(start,end); i++) {
+      numbers.push(Math.min(start, end) + i)
+    }
+  }
+  let result = numbers.reduce((previousValue, currentValue) => previousValue + currentValue);
+  return result
 
 };
 
