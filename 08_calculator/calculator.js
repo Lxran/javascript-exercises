dba8fb06c4b78acb2adbbd3cb04eb67c540bef47
Lxran <lxranman@gmail.com>
2022-10-08 15:48:32 +0300

    completed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	if (array.length < 1) {
+    return 0
+  }
+  let result = array.reduce((previousValue, currentValue) => previousValue + currentValue);
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let result = nums.reduce((a, b) => a * b, 1);
+  return result 
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	let result = base ** power;
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = num;
+  if (num === 0 || num === 1) 
+    return 1; 
+  while (num > 1) { 
+    num--;
+    result *= num;
+  }
+  return result;
 };
 
 // Do not edit below this line
