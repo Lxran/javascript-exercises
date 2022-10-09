f166319469e627466d2607352f4c68093abf447b
Lxran <lxranman@gmail.com>
2022-10-09 11:13:23 +0300

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -3,7 +3,7 @@ const fibonacci = function (index) {
   Number.parseInt(index);
   if (index < 0) return "OOPS";
   if (index === 1 || index === 2) return 1
-  for (let i = index; i >= fibonacci.length; i--) {
+  for (let i = index; i > 2; i--) {
     let newNum = fibonacci.at(-1) + fibonacci.at(-2);
     fibonacci.push(newNum);
   }
