944b34b13daf335c0a9232da905d47646ca5862b
Lxran <lxranman@gmail.com>
2022-10-09 10:54:12 +0300

    needs tinkering

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,17 +1,13 @@
 const fibonacci = function (index) {
   let fibonacci = [1, 1];
-  index = Number.parseInt(index);
+  Number.parseInt(index);
   if (index < 0) return "OOPS";
   if (index === 1 || index === 2) return 1
-  if (index >= 3) {
-    for (let i = index; i > fibonacci.length; i--) {
-      let num1 = fibonacci[-1];
-      let num2 = fibonacci[-2];
-      let newNum = num1 + num2;
-      fibonacci.push(newNum);
-    }
-    return fibonacci[-1];
+  for (let i = index; i >= fibonacci.length; i--) {
+    let newNum = fibonacci.at(-1) + fibonacci.at(-2);
+    fibonacci.push(newNum);
   }
+  return fibonacci.at(-1);
 };
 
 // Do not edit below this line
